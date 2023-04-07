0c127f1f143fe89d6bffcc3b6f1919497162e909
Ekaterina Bogdanova <katarina.bog@icloud.com>
2023-04-07 21:07:24 +0200

    Code button

--- FourthProject/ViewController.swift
+++ FourthProject/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var logInButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        logInButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func logInButtonTapped () {
+        guard let inputText = userNameTextField.text, !inputText.isEmpty else {
+            showAlert(withTitle: "Text field is empty", andMessage: "Please enter your name")
+            return
+        }
+        
+        let userNamePattern = "<[a-zA-Z]{3,20}$"
+        let isUserNameValid = NSPredicate(format: "SELF MATCHES %@", userNamePattern).evaluate(with: inputText)
+        if !isUserNameValid {
+            showAlert(withTitle: "Wrong format", andMessage: "Please enter your name")
+        }
+        userNameTextField.text = ""
+    }
+    
+    
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.userNameTextField.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
-
